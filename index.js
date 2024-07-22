b5d2385247cf57db51a4665f155734796f7dd4db
denismnjima <denismnjima@gmail.com>
2024-07-22 11:55:02 +0300

    solved question

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+// helpers.js
+class Animal {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  }
+  
+  class Cat extends Animal {
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog extends Animal {
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird extends Animal {
+    speak() {
+      if (this.name === 'Pablo') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  module.exports = {
+    Cat,
+    Dog,
+    Bird,
+  };
+  
